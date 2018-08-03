dbf3a355876530afffc4d52f9dbe1465dbd11ef1
jicg <284077319@qq.com>
2018-08-03 11:27:25 +0800

    docker-修改 Dockerfile

---
+++ routers/commentsRouter_controllers.go
@@ -0,0 +1,162 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:FroalaController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:FroalaController"],
+		beego.ControllerComments{
+			Method: "UploadFile",
+			Router: `/uploadfile`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:FroalaController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:FroalaController"],
+		beego.ControllerComments{
+			Method: "UploadImg",
+			Router: `/uploadimg`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:FroalaController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:FroalaController"],
+		beego.ControllerComments{
+			Method: "UploadVideo",
+			Router: `/uploadvideo`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "Get",
+			Router: `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "GetAbout",
+			Router: `/about`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "GetComment",
+			Router: `/comment/:key`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "GetDetail",
+			Router: `/details/:key`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "GetMessage",
+			Router: `/message`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "GetReg",
+			Router: `/reg`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:IndexController"],
+		beego.ControllerComments{
+			Method: "GetUser",
+			Router: `/user`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:MessageController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:MessageController"],
+		beego.ControllerComments{
+			Method: "NewMessage",
+			Router: `/new/?:key`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"],
+		beego.ControllerComments{
+			Method: "Del",
+			Router: `/del/:key`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"],
+		beego.ControllerComments{
+			Method: "EditPage",
+			Router: `/edit/:key`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"],
+		beego.ControllerComments{
+			Method: "NewPage",
+			Router: `/new`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:NoteController"],
+		beego.ControllerComments{
+			Method: "Save",
+			Router: `/save/:key`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:PraiseController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:PraiseController"],
+		beego.ControllerComments{
+			Method: "Parse",
+			Router: `/:type/:key`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Login",
+			Router: `/login`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Logout",
+			Router: `/logout`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+	beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/jicg/liteblog/controllers:UserController"],
+		beego.ControllerComments{
+			Method: "Reg",
+			Router: `/reg`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams: param.Make(),
+			Params: nil})
+
+}
